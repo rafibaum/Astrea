e5767218d937f6eefa9c75c6505b7d97d5e07ecd
Rafi Baum <rafi@ukbaums.com>
2019-11-25 21:54:35 +0000

    Delegate functionality to separate method

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use tokio::net::TcpListener;
-use tokio::net::TcpStream;
-use tokio::prelude::*;
+use serde::Deserialize;
 use std::fs::File;
 use std::net::IpAddr;
-use serde::Deserialize;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
 
 #[derive(Debug, Deserialize)]
 struct AstreaConfig {
     host: IpAddr,
     port: u16,
-    endpoints: Vec<(IpAddr, u16)>
+    endpoints: Vec<(IpAddr, u16)>,
 }
 
 #[tokio::main]
@@ -21,53 +21,40 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     loop {
         let (client_sock, _) = listener.accept().await?;
-        let endpoint = config.endpoints.first().unwrap().clone();
+        let endpoint = *config.endpoints.first().unwrap();
 
         tokio::spawn(async move {
             let server_sock = TcpStream::connect(endpoint).await.unwrap();
 
-            let (mut client_read, mut client_write) = tokio::io::split(client_sock);
-            let (mut server_read, mut server_write) = tokio::io::split(server_sock);
+            let (client_read, client_write) = tokio::io::split(client_sock);
+            let (server_read, server_write) = tokio::io::split(server_sock);
 
-            tokio::spawn(async move {
-                let mut buf = [0; 1024];
-
-                loop {
-                    let n = match client_read.read(&mut buf).await {
-                        Ok(n) if n == 0 => return,
-                        Ok(n) => n,
-                        Err(e) => {
-                            println!("failed to read from socket, err = {:?}", e);
-                            return;
-                        }
-                    };
-
-                    if let Err(e) = server_write.write_all(&buf[0..n]).await {
-                        println!("failed to write to socket; err = {:?}", e);
-                        return;
-                    }
-                }
-            });
-
-            tokio::spawn(async move {
-                let mut buf = [0; 1024];
-
-                loop {
-                    let n = match server_read.read(&mut buf).await {
-                        Ok(n) if n == 0 => return,
-                        Ok(n) => n,
-                        Err(e) => {
-                            println!("failed to read from socket, err = {:?}", e);
-                            return;
-                        }
-                    };
-
-                    if let Err(e) = client_write.write_all(&buf[0..n]).await {
-                        println!("failed to write to socket; err = {:?}", e);
-                        return;
-                    }
-                }
-            });
+            tokio::spawn(copy(client_read, server_write));
+            tokio::spawn(copy(server_read, client_write));
         });
+    }
+}
+
+async fn copy<R, W>(mut reader: R, mut writer: W)
+where
+    R: AsyncReadExt + Unpin,
+    W: AsyncWriteExt + Unpin,
+{
+    let mut buf = [0; 1024];
+
+    loop {
+        let n = match reader.read(&mut buf).await {
+            Ok(n) if n == 0 => return,
+            Ok(n) => n,
+            Err(e) => {
+                println!("failed to read from socket, err = {:?}", e);
+                return;
+            }
+        };
+
+        if let Err(e) = writer.write_all(&buf[0..n]).await {
+            println!("failed to write to socket; err = {:?}", e);
+            return;
+        }
     }
 }
