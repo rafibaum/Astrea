64bb9b49b141ff9ba57dfc9dfff9179a8cf88713
Rafi Baum <rafi@ukbaums.com>
2019-11-25 23:18:14 +0000

    Made endpoint selection behaviour generic

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,26 @@ use tokio::net::TcpStream;
 struct AstreaConfig {
     host: IpAddr,
     port: u16,
-    endpoints: VecDeque<(IpAddr, u16)>,
+    endpoints: Vec<(IpAddr, u16)>,
+    #[serde(rename = "endpoint-selector")]
+    endpoint_selector: EndpointSelectors,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config_file = File::open("astrea.yml").unwrap();
-    let mut config: AstreaConfig = serde_yaml::from_reader(config_file).unwrap();
+    let config: AstreaConfig = serde_yaml::from_reader(config_file).unwrap();
+    let mut endpoint_selector: Box<dyn EndpointSelector> = match config.endpoint_selector {
+        EndpointSelectors::RoundRobin => {
+            Box::new(RoundRobin::new(VecDeque::from(config.endpoints)))
+        }
+    };
 
     let mut listener = TcpListener::bind((config.host, config.port)).await?;
 
     loop {
         let (client_sock, _) = listener.accept().await?;
-        let endpoint = config.endpoints.pop_front().unwrap();
-        config.endpoints.push_back(endpoint);
+        let endpoint = endpoint_selector.next();
 
         tokio::spawn(async move {
             let server_sock = TcpStream::connect(endpoint).await.unwrap();
@@ -44,5 +50,34 @@ where
 {
     if let Err(e) = reader.copy(&mut writer).await {
         println!("Socket broken: {:?}", e);
+    }
+}
+
+#[derive(Debug, Deserialize)]
+enum EndpointSelectors {
+    #[serde(alias = "round robin")]
+    RoundRobin,
+}
+
+trait EndpointSelector {
+    fn next(&mut self) -> (IpAddr, u16);
+}
+
+struct RoundRobin {
+    endpoints: VecDeque<(IpAddr, u16)>,
+}
+
+impl RoundRobin {
+    fn new(endpoints: VecDeque<(IpAddr, u16)>) -> RoundRobin {
+        assert!(!endpoints.is_empty());
+        RoundRobin { endpoints }
+    }
+}
+
+impl EndpointSelector for RoundRobin {
+    fn next(&mut self) -> (IpAddr, u16) {
+        let result = self.endpoints.pop_front().unwrap();
+        self.endpoints.push_back(result);
+        result
     }
 }
